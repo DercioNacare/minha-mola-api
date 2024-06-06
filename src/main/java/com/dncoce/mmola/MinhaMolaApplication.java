59132c9e9d07091563972f3c130780949116aee6
Dercio Nacare <dpvnacare@gmail.com>
2024-06-06 13:09:15 +0200

    Criação do projecto

---
+++ src/main/java/com/dncoce/mmola/MinhaMolaApplication.java
@@ -0,0 +1,12 @@
+package com.dncoce.mmola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaMolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaMolaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dncoce/mmola/MinhaMolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dncoce.mmola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaMolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
